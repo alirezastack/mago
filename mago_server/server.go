afadb5d16409891f4bccb05d2f55f34fa981d4aa
alireza <alireza.stack@gmail.com>
2021-03-25 00:45:10 +0430

    Enable TLS encryption with sample TLS files, add --port flag
    helpers/ssl folder keeps sample TLS files and also ssl generator shell script
    --port flag is used to run Mago server on a different port rather than 50051

--- mago_server/server.go
+++ mago_server/server.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	mago "github.com/alirezastack/mago/magopb"
 	"github.com/nyaruka/phonenumbers"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
 )
 
+var port = flag.Int("port", 50051, "gRPC Listening Port")
+
 type server struct {
 	mago.UnimplementedMagoServiceServer
 }
 
-// CreateUser implements mago.CreateUser
 func (s *server) CreateUser(ctx context.Context, in *mago.CreateUserRequest) (*mago.CreateUserResponse, error) {
 	log.Printf("Received user phone: %v", in.GetPhone())
+	// We just check client cancellation before expensive calls
+	//if ctx.Err() == context.Canceled {
+	//	fmt.Println("The client canceled the request!")
+	//	return nil, status.Error(codes.Canceled, "The client canceled the request!")
+	//}
+
 	num, err := phonenumbers.Parse(in.GetPhone(), "IR")
 	formattedNumber := phonenumbers.Format(num, phonenumbers.E164)
 	log.Printf("International Phone Number: %v", formattedNumber)
@@ -34,14 +43,24 @@ func (s *server) CreateUser(ctx context.Context, in *mago.CreateUserRequest) (*m
 }
 
 func main() {
-	fmt.Println("Starting up Mago server...")
+	flag.Parse()
+	fmt.Printf("Starting up Mago server on 0.0.0.0:%v...\n", *port)
 
-	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	// initialize encryption
+	certFile := "helpers/ssl/server.crt"
+	keyFile := "helpers/ssl/server.pem"
+	creds, certErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if certErr != nil {
+		log.Fatalf("Failed loading certificates: %v", certErr)
+	}
+
+	opts := grpc.Creds(creds)
+	s := grpc.NewServer(opts)
 	mago.RegisterMagoServiceServer(s, &server{})
 
 	fmt.Println("Mago server is up & running ;)")
